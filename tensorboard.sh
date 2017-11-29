b3d6d3904f318809e65a7c2c507c0d0f30a54332
BasicCoder <abasiccoder@gmail.com>
2017-11-29 21:01:48 +0800

    first commit to init repository

---
+++ tensorboard.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+LC_ALL="en_US.UTF-8" tensorboard --logdir=./resnet_model --port=8008
---
+++ train_resnet.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+CUDA_VISIBLE_DEVICES=0 python imagenet_main.py --data_dir=./data
